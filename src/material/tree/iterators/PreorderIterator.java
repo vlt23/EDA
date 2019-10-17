bdadccb1419cc079dcc4ec6c8cdcb29ecb60b3ab
vlt23 <vlt23@protonmail.com>
2019-10-17 12:08:00 +0200

    PreorderIterator: initial implementation

--- src/material/tree/iterators/PreorderIterator.java
+++ src/material/tree/iterators/PreorderIterator.java
@@ -3,7 +3,9 @@ package material.tree.iterators;
 import material.Position;
 import material.tree.Tree;
 
+import java.util.Deque;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.function.Predicate;
 
 /**
@@ -11,31 +13,51 @@ import java.util.function.Predicate;
  *
  * @param <E>
  * @author A. Duarte, J. Vélez, J. Sánchez-Oro, JD. Quintana
+ * @author vlt23
  */
-//TODO: Practica 2 Ejercicio 3
+
 public class PreorderIterator<E> implements Iterator<Position<E>> {
 
+    private Deque<Position<E>> nodeStack;
+    private final Tree<E> tree;
+
     public PreorderIterator(Tree<E> tree) {
-        throw new RuntimeException("Not yet implemented");
+        nodeStack = new LinkedList<>();
+        this.tree = tree;
+        nodeStack.addFirst(tree.root());
     }
 
     public PreorderIterator(Tree<E> tree, Position<E> start) {
-        throw new RuntimeException("Not yet implemented");
+        nodeStack = new LinkedList<>();
+        this.tree = tree;
+        nodeStack.addFirst(start);
     }
 
     public PreorderIterator(Tree<E> tree, Position<E> start, Predicate<Position<E>> predicate) {
         throw new RuntimeException("Not yet implemented");
     }
 
+    private void _preOrder(Position<E> p) {
+        Iterable<Position<E>> childrenI = (Iterable<Position<E>>) tree.children(p);  // TODO
+        Deque<Position<E>> children = new LinkedList<>();
+        for (Position<E> ePosition : childrenI) {
+            children.addFirst(ePosition);
+        }
+        while (!children.isEmpty()) {
+            nodeStack.addFirst(children.removeFirst());
+        }
+    }
 
     @Override
     public boolean hasNext() {
-        throw new RuntimeException("Not yet implemented");
+        return !nodeStack.isEmpty();
     }
 
     @Override
     public Position<E> next() {
-        throw new RuntimeException("Not yet implemented");
+        Position<E> aux = nodeStack.removeFirst();
+        _preOrder(aux);
+        return aux;
     }
 
 }
