620eb31048a019165b75ca12d4c8c384ce13e319
vlt23 <vlt23@protonmail.com>
2019-11-27 19:19:39 +0100

    FlightManager: initial implementation

--- src/usecase/FlightManager.java
+++ src/usecase/FlightManager.java
@@ -1,28 +1,39 @@
 package usecase;
 
+import material.maps.HashTableMapDH;
 
 public class FlightManager {
 
+    private HashTableMapDH<Integer, Flight> flightsMap;
 
     public Flight addFlight(String company, int flightCode, int year, int month, int day) {
-        throw new RuntimeException("Not yet implemented.");
+        Flight flight = new Flight();
+        flight.setCompany(company);
+        flight.setFlightCode(flightCode);
+        flight.setDate(year, month, day);
+        flightsMap.put(calculateKey(company, flightCode, year, month, day), flight);
+        return flight;
     }
 
     public Flight getFlight(String company, int flightCode, int year, int month, int day) {
-        throw new RuntimeException("Not yet implemented.");
+        return flightsMap.get(calculateKey(company, flightCode, year, month, day));
     }
 
-
     public void updateFlight(String company, int flightCode, int year, int month, int day, Flight updatedFlightInfo) {
-        throw new RuntimeException("Not yet implemented.");
-
-
+        Flight flight = flightsMap.get(calculateKey(company, flightCode, year, month, day));
+        flight.setCapacity(updatedFlightInfo.getCapacity());
+        flight.setOrigin(updatedFlightInfo.getOrigin());
+        flight.setDestination(updatedFlightInfo.getDestination());
+        flight.setDelay(updatedFlightInfo.getDelay());
+        Iterable<String> properties = updatedFlightInfo.getAllAttributes();
+        for (String property : properties) {
+            flight.setProperty(property, updatedFlightInfo.getProperty(property));
+        }
     }
 
     public void addPassenger(String dni, String name, String surname, Flight flight) {
         throw new RuntimeException("Not yet implemented.");
     }
-
 
     public Iterable<Passenger> getPassengers(String company, int flightCode, int year, int month, int day) {
         throw new RuntimeException("Not yet implemented.");
@@ -39,6 +50,11 @@ public class FlightManager {
     public Iterable<Flight> getFlightsByDestination(String destination, int year, int month, int day) {
         throw new RuntimeException("Not yet implemented.");
 
+    }
+
+    private int calculateKey(String company, int flightCode, int year, int month, int day) {
+        return company.hashCode() + Integer.hashCode(flightCode)
+                + Integer.hashCode(year) + Integer.hashCode(month) + Integer.hashCode(day);
     }
 
 }
