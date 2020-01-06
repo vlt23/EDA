e3fb9afa58242ca785737ef8d0d68a4e2b71bbd9
vlt23 <vlt23@protonmail.com>
2020-01-06 18:45:28 +0100

    DefaultComparator: reformat code

--- src/material/tree/binarysearchtree/DefaultComparator.java
+++ src/material/tree/binarysearchtree/DefaultComparator.java
@@ -2,21 +2,22 @@ package material.tree.binarysearchtree;
 
 import java.util.Comparator;
 
-/** Comparator based on the natural ordering
-  *
-  *  @author R. Cabido, A. Duarte and J. Vélez
-  */
+/**
+ * Comparator based on the natural ordering
+ *
+ * @author R. Cabido, A. Duarte and J. Vélez
+ */
 
 public class DefaultComparator<E> implements Comparator<E> {
-  /** Compares two given elements
-    *
-    * @return a negative integer if <tt>a</tt> is less than <tt>b</tt>,
-    * zero if <tt>a</tt> equals <tt>b</tt>, or a positive integer if
-    * <tt>a</tt> is greater than <tt>b</tt>
-    */
+    /**
+     * Compares two given elements
+     *
+     * @return a negative integer if <tt>a</tt> is less than <tt>b</tt>,
+     * zero if <tt>a</tt> equals <tt>b</tt>, or a positive integer if
+     * <tt>a</tt> is greater than <tt>b</tt>
+     */
+    public int compare(E a, E b) throws ClassCastException {
+        return ((Comparable<E>) a).compareTo(b);
+    }
 
-	public int compare(E a, E b) throws ClassCastException { 
-    return ((Comparable<E>) a).compareTo(b);
-  }
 }
-
