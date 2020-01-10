4c0ac243bb8e5b07b6aab78a6f0b226697d6da6b
vlt23 <vlt23@protonmail.com>
2020-01-10 14:33:25 +0100

    InorderBinaryTreeIterator: fix when the initial node isn't root

--- src/material/tree/iterators/InorderBinaryTreeIterator.java
+++ src/material/tree/iterators/InorderBinaryTreeIterator.java
@@ -11,22 +11,34 @@ import java.util.LinkedList;
  * Inorder iterator for binary trees.
  *
  * @param <T>
- * @author jvelez, JD. Quintana
+ * @author jvelez, JD. Quintana, vlt23
  */
 public class InorderBinaryTreeIterator<T> implements Iterator<Position<T>> {
 
     private Deque<Position<T>> nodeStack = new LinkedList<>();
     private final BinaryTree<T> tree;
+    private boolean isBeginRoot;
+    private final Position<T> initialPosition;
 
     public InorderBinaryTreeIterator(BinaryTree<T> tree) {
         this.tree = tree;
-        if (!tree.isEmpty())
+        if (!tree.isEmpty()) {
             goToLastInLeft(tree.root());
+        }
+        isBeginRoot = true;
+        initialPosition = null;  // Not necessary for this constructor
     }
 
     public InorderBinaryTreeIterator(BinaryTree<T> tree, Position<T> node) {
         this.tree = tree;
         goToLastInLeft(node);
+        if (node != tree.root()) {
+            initialPosition = node;
+            isBeginRoot = false;
+        } else {
+            initialPosition = null;
+            isBeginRoot = true;
+        }
     }
 
     private void goToLastInLeft(Position<T> node) {
@@ -50,6 +62,22 @@ public class InorderBinaryTreeIterator<T> implements Iterator<Position<T>> {
         Position<T> aux = nodeStack.removeFirst();
         if (tree.hasRight(aux)) {
             goToLastInLeft(tree.right(aux));
+        } else if (!isBeginRoot && nodeStack.isEmpty()) {
+            Position<T> parent = tree.parent(initialPosition);
+            Position<T> child = initialPosition;
+            boolean ifAdd = true;  // need to not iterate twice
+            while (tree.hasRight(parent) && tree.right(parent) == child) {
+                if (tree.root() == parent) {
+                    ifAdd = false;
+                    break;
+                }
+                parent = tree.parent(parent);
+                child = tree.parent(child);
+            }
+            if (ifAdd) {
+                nodeStack.addFirst(parent);
+            }
+            isBeginRoot = true;
         }
         return aux;
     }
